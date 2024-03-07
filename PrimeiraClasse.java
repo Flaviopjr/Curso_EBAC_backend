76561c67b0498ae62aaf115976359855ecedaeeb
Flaviopjr <155128243+Flaviopjr@users.noreply.github.com>
2024-03-07 01:28:55 -0300

    Alteração do nome apresentado na tela

--- PrimeiraClasse.java
+++ PrimeiraClasse.java
@@ -3,5 +3,6 @@ package br.com.projetoebac;
 public class PrimeiraClasse {
 
 	public static void main(String[] args) {
-		System.out.print("HelloWorld");}
+		System.out.print("Hello Flávio");
+	}
 }
